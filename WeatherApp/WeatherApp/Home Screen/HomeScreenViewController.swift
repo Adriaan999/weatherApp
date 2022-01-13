ddba98edec27fbdf34269e0175c6bf0a1fe3ac55
Adriaan van Schalkwyk <Adriaan9@hotmail.co.za>
2022-01-13 12:02:12 +0200

    add initial table view cell setup

--- WeatherApp/WeatherApp/Home Screen/HomeScreenViewController.swift
+++ WeatherApp/WeatherApp/Home Screen/HomeScreenViewController.swift
@@ -8,5 +8,42 @@
 import UIKit
 
 class HomeScreenViewController: UIViewController {
+ 
+    @IBOutlet public var weatherTableView: UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupTableViewCell()
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
+    }
+    
+    private func setupTableViewCell() {
+        self.weatherTableView.register(UINib(nibName: "WeatherTableViewCell", bundle: Bundle.main),
+                                forCellReuseIdentifier: "WeatherTableViewCell")
+        
+        self.weatherTableView.register(UINib(nibName: "WeatherTableViewHeaderCell", bundle: Bundle.main),
+                                forCellReuseIdentifier: "WeatherTableViewHeaderCell")
+    }
+    
+}
+
+extension HomeScreenViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherTableViewCell", for: indexPath) as! WeatherTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerCell = tableView.dequeueReusableCell(withIdentifier: "WeatherTableViewHeaderCell") as! WeatherTableViewHeaderCell
+        return headerCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
---
+++ WeatherApp/WeatherApp/Home Screen/Weather Cells/Header Cells/WeatherTableViewHeaderCell.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherTableViewHeaderCell.swift
+//  WeatherApp
+//
+//  Created by Adriaan van Schalkwyk on 2022/01/13.
+//
+
+import UIKit
+
+class WeatherTableViewHeaderCell: UITableViewCell {
+    
+}
